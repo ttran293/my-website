98aac779ae392d99a0f23622464ab2630e38015b
Thanh Nam Tran <ttran19@umbc.edu>
2021-08-23 02:38:42 -0400

    wildcard selector beta test

--- dropdown-menu.js
+++ dropdown-menu.js
@@ -1,19 +1,21 @@
+
+
 function f() {
-  document.getElementsByClassName('dropdown')[0].classList.toggle('down');
-  document.getElementsByClassName('arrow')[0].classList.toggle('gone');
-  if (document.getElementsByClassName('dropdown')[0].classList.contains('down')) {
+  document.getElementsByClassName('dropdown-1')[0].classList.toggle('down');
+  document.getElementsByClassName('arrow-1')[0].classList.toggle('gone');
+  if (document.getElementsByClassName('dropdown-1')[0].classList.contains('down')) {
     setTimeout(function() {
-      document.getElementsByClassName('dropdown')[0].style.overflow = 'visible'
+      document.getElementsByClassName('dropdown-1')[0].style.overflow = 'visible'
     }, 500)
   } else {
-    document.getElementsByClassName('dropdown')[0].style.overflow = 'hidden'
+    document.getElementsByClassName('dropdown-1')[0].style.overflow = 'hidden'
   }
 }
 
 function f2() {
-  document.getElementsByClassName('dropdown-2')[0].classList.toggle('down-2');
-  document.getElementsByClassName('arrow-2')[0].classList.toggle('gone-2');
-  if (document.getElementsByClassName('dropdown-2')[0].classList.contains('down-2')) {
+  document.getElementsByClassName('dropdown-2')[0].classList.toggle('down');
+  document.getElementsByClassName('arrow-2')[0].classList.toggle('gone');
+  if (document.getElementsByClassName('dropdown-2')[0].classList.contains('down')) {
     setTimeout(function() {
       document.getElementsByClassName('dropdown-2')[0].style.overflow = 'visible'
     }, 500)
