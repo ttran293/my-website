3fd6f18f7451038b9f502bb1ae39e96c9b2ca901
Thanh Nam Tran <ttran19@umbc.edu>
2021-08-23 03:21:42 -0400

    html skills done, 2 bugs need to be fixed

--- dropdown-menu.js
+++ dropdown-menu.js
@@ -23,3 +23,48 @@ function f2() {
     document.getElementsByClassName('dropdown-2')[0].style.overflow = 'hidden'
   }
 }
+
+function f3() {
+  document.getElementsByClassName('dropdown-3')[0].classList.toggle('down');
+  document.getElementsByClassName('arrow-3')[0].classList.toggle('gone');
+  if (document.getElementsByClassName('dropdown-3')[0].classList.contains('down')) {
+    setTimeout(function() {
+      document.getElementsByClassName('dropdown-3')[0].style.overflow = 'visible'
+    }, 500)
+  } else {
+    document.getElementsByClassName('dropdown-3')[0].style.overflow = 'hidden'
+  }
+}
+function f4() {
+  document.getElementsByClassName('dropdown-4')[0].classList.toggle('down');
+  document.getElementsByClassName('arrow-4')[0].classList.toggle('gone');
+  if (document.getElementsByClassName('dropdown-4')[0].classList.contains('down')) {
+    setTimeout(function() {
+      document.getElementsByClassName('dropdown-4')[0].style.overflow = 'visible'
+    }, 500)
+  } else {
+    document.getElementsByClassName('dropdown-4')[0].style.overflow = 'hidden'
+  }
+}
+function f5() {
+  document.getElementsByClassName('dropdown-5')[0].classList.toggle('down');
+  document.getElementsByClassName('arrow-5')[0].classList.toggle('gone');
+  if (document.getElementsByClassName('dropdown-5')[0].classList.contains('down')) {
+    setTimeout(function() {
+      document.getElementsByClassName('dropdown-5')[0].style.overflow = 'visible'
+    }, 500)
+  } else {
+    document.getElementsByClassName('dropdown-5')[0].style.overflow = 'hidden'
+  }
+}
+function f6() {
+  document.getElementsByClassName('dropdown-6')[0].classList.toggle('down');
+  document.getElementsByClassName('arrow-6')[0].classList.toggle('gone');
+  if (document.getElementsByClassName('dropdown-6')[0].classList.contains('down')) {
+    setTimeout(function() {
+      document.getElementsByClassName('dropdown-6')[0].style.overflow = 'visible'
+    }, 500)
+  } else {
+    document.getElementsByClassName('dropdown-6')[0].style.overflow = 'hidden'
+  }
+}
